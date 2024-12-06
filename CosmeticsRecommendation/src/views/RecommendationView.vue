b97a7ed101d05fcb87dba3d5676c1f3e411f1907
gilanggnw <gilanggunawansaputra@gmail.com>
2024-12-06 23:53:01 +0700

    Changed API Link

--- CosmeticsRecommendation/src/views/RecommendationView.vue
+++ CosmeticsRecommendation/src/views/RecommendationView.vue
@@ -16,7 +16,7 @@ const itemsPerPage = 30
 
 const fetchProducts = async () => {
   try {
-    const response = await fetch('http://localhost:5000/api/products')
+    const response = await fetch('https://loveyourskin.vercel.app/api/products')
     const data = await response.json()
     products.value = data
   } catch (error) {
